c0795b66ade16dd191691f7b978498cc4cbc0002
SoYoung <itsso0621@naver.com>
2022-09-20 20:33:34 -0400

    light-mode

--- portfolio/public/javascripts/index.js
+++ portfolio/public/javascripts/index.js
@@ -33,6 +33,12 @@ function pageTransitions() {
       element.classList.add("active");
     }
   });
+
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 
 pageTransitions();
